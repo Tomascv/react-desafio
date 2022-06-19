2cbdd5f80901623d3bb500cfee8c1479cd0bc251
Tomas Cavenaghi <tscavenaghi@gmail.com>
2022-06-19 09:58:47 -0300

    desafio

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Test1 from './Desafiotest'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+   <Test1 nombre='Lector'/>
   </React.StrictMode>
 );
 
